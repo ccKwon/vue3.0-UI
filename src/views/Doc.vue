7f09ab547ded5b87452e6e6bc0e16afcc6163346
cckwon <308418835@qq.com>
2021-07-25 19:57:31 +0800

    Topnav封装

---
+++ src/views/Doc.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <Topnav/>
+
+
+    <div class="content">
+      <aside>边栏</aside>
+      <main>主内容</main>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+    import Topnav from "../components/Topnav.vue"
+export default {
+    components: {
+        Topnav
+    }
+}
+</script>
+
+
+<style lang="scss">
+$aside-index: 10;
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: white;
+  }
+}
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+  z-index: $aside-index;
+  > h2 {
+    margin-bottom: 4px;
+    padding: 0 16px;
+  }
+  > ol {
+    > li {
+      > a {
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
+      }
+    }
+  }
+}
+main {
+  overflow: auto;
+}
+</style>
